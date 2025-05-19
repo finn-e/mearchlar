e32153233781d8336a7834d288addcade5c74612
Fin o'Flaherty <fin@oflaherty.is>
2025-05-19 11:31:47 -0400

    Added 5-way and Azoteq Trackpad footprints Put them on board. Should be easier than modifying it in KiCad every time.

---
+++ ergogen/footprints/azoteq-jst.js
@@ -0,0 +1,4 @@
+    (fp_line (start -6.41 15.37) (end -6.41 18.03) (layer "F.SilkS") (stroke (width 0.12) (type solid)))
+    (fp_line (start 6.41 18.03) (end -6.41 18.03) (layer "F.SilkS") (stroke (width 0.12) (type solid)))
+    (fp_line (start 6.41 15.37) (end 6.41 18.03) (layer "F.SilkS") (stroke (width 0.12) (type solid)))
+    (fp_line (start 6.41 15.37) (end -6.41 15.37) (layer "F.SilkS") (stroke (width 0.12) (type solid)))
